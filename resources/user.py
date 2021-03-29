2a47a5bdb7895b4916b6db086da813467e453f09
NeryCaballero <nerycaballero24@gmail.com>
2021-03-29 22:49:11 +0200

    Added User resource: GET user by id, DELETE user by id

--- resources/user.py
+++ resources/user.py
@@ -26,4 +26,21 @@ class UserRegister(Resource):
         user = UserModel(**data)
         user.save_to_db()
 
-        return {"message": "User created successfully."}, 201
+        return {"message": "User created successfully.", "id": user.id}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User Not Found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User Not Found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted.'}
