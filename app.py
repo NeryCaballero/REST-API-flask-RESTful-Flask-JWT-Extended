be19918df54b9ebbc7dc57de3d763a949620001e
NeryCaballero <nerycaballero24@gmail.com>
2021-04-02 22:13:17 +0200

    Replaced flask_jwt by flask_jwt_extended, Added UserLogin Resource.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from db import db
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -15,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   # allows to return custom errors
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-app.secret_key = 'nery'
+app.config['JWT_SECRET_KEY'] = 'nery'
 api = Api(app)
 
 
@@ -24,7 +23,7 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -34,6 +33,7 @@ api.add_resource(ItemList, '/items')
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
